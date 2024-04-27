665c403938f01980b365acdda1b23a441e756691
Himanshu Sharma <himanshu.sharma.ext@heromotocorp.com>
2024-04-27 18:12:15 +0530

    Lecture 12: Ajax Code with Core JS, get Method and Ajax methods.

--- jqexpress/eg2.js
+++ jqexpress/eg2.js
@@ -3,10 +3,46 @@ const oracle = require('oracledb');
 const app = express();
 const port = 3000;
 
+class Employee {
+    constructor(id, fn, ln){
+        this.id = id; 
+        this.firstName = fn;
+        this.lastName = ln;
+    }
+    getId(){return this.id; }
+    getFirstName(){return this.firstName; }
+    getLastName(){return this.lastName; }
+}
+
 app.use(express.static("learn")); // to read static data from learn directory
 app.get("/", function(request, response){
     response.redirect('/index.html');
 });
+
+app.get("/getEmployees", async function(request, response){
+    var connection = null;
+    try {
+        connection = await oracle.getConnection({
+            'user':'hr',
+            'password':'hr',
+            'connectionString':'//localhost:1521/xepdb1'
+        });
+        let resultset = await connection.execute('Select employee_id, first_name, last_name FROM employees ORDER BY first_name');
+        var i = 0;
+        var emps=[];
+        var emp;
+        while(i<resultset.rows.length){
+            emp = new Employee(resultset.rows[i][0],resultset.rows[i][1],resultset.rows[i][2]);
+            emps.push(emp);
+            i++;
+        }
+        await connection.close();
+        response.send(emps);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
 app.get('/employees', async function(req, res){
     let connection = null;
     try {
